1bb05ef01a65ba1287f1fa433d94c9fbbaf698b9
Bagasbanuna02 <121874064+Bagasbanuna02@users.noreply.github.com>
2023-05-25 13:40:13 +0800

    Create PenerapaniF_didalam_Component.ts

---
+++ Tsx/PenerapaniF_didalam_Component.ts
@@ -0,0 +1,8 @@
+{(() =>{})()}
+
+ex: 
+          {(() => {
+            let data = 2;
+            if (data == 1) return <Title>Tidak</Title>;
+            return <Title>Ini Disini</Title>;
+          })()}
