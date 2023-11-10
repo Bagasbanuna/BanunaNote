e2b15c1d841b529903047732d83310fe74c124b1
Bagasbanuna02 <121874064+Bagasbanuna02@users.noreply.github.com>
2023-11-10 10:28:14 +0800

    Create file_type.ts

---
+++ file_type.ts
@@ -0,0 +1,28 @@
+
+
+const fileTypes = [{ name: "png", mimeType: "image/png", extension: "png" },
+{ name: "jpg", mimeType: "image/jpeg", extension: "jpg" },
+{ name: "jpeg", mimeType: "image/jpeg", extension: "jpeg" },
+{ name: "gif", mimeType: "image/gif", extension: "gif" },
+{ name: "bmp", mimeType: "image/bmp", extension: "bmp" },
+{ name: "svg", mimeType: "image/svg+xml", extension: "svg" },
+{ name: "mp3", mimeType: "audio/mpeg", extension: "mp3" },
+{ name: "ogg", mimeType: "audio/ogg", extension: "ogg" },
+{ name: "wav", mimeType: "audio/wav", extension: "wav" },
+{ name: "mp4", mimeType: "video/mp4", extension: "mp4" },
+{ name: "webm", mimeType: "video/webm", extension: "webm" },
+{ name: "ogv", mimeType: "video/ogg", extension: "ogv" },
+{ name: "txt", mimeType: "text/plain", extension: "txt" },
+{ name: "html", mimeType: "text/html", extension: "html" },
+{ name: "css", mimeType: "text/css", extension: "css" },
+{ name: "json", mimeType: "application/json", extension: "json" },
+{ name: "pdf", mimeType: "application/pdf", extension: "pdf" },
+{ name: "zip", mimeType: "application/zip", extension: "zip" },
+{ name: "bin", mimeType: "application/octet-stream", extension: "bin" },
+{ name: "doc", mimeType: "application/msword", extension: "doc" },
+{ name: "sh", mimeType: "application/x-sh", extension: "sh" },
+{ name: "js", mimeType: "application/javascript", extension: "js" },
+{ name: "ts", mimeType: "application/typescript", extension: "ts" },
+{ name: "docx", mimeType: "application/vnd.openxmlformats-officedocument.wordprocessingml.document", extension: "docx" }];
+
+export default fileTypes
